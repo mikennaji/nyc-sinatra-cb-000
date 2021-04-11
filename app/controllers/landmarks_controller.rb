74f95d4f0a52e49580074b051d92189447134a89
Michael Nnaji <mnnaji3@uwo.ca>
2021-04-11 22:33:46 +0000

    Automatically backed up by Learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -3,4 +3,9 @@ class LandmarksController < ApplicationController
     get '/landmarks/new' do
       erb :'/landmarks/new'
     end
+
+    post 'landmarks' do 
+      @landmark = Landmark.create(params[:landmark])
+      erb
+    end  
 end
