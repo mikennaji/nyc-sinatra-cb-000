b4852f86d013ef8b4f29601c02882dd32a05a4d3
Michael Nnaji <mnnaji3@uwo.ca>
2021-04-11 19:40:00 +0000

    Automatically backed up by Learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,6 +1,6 @@
 class LandmarksController < ApplicationController
   # add controller methods
-    get '/landmarks/new' do 
+    get '/landmarks/new' do
       erb :'/landmarks/new'
-    end 
+    end
 end
