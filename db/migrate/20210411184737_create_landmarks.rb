a8efe11aca65d71fc2df8eb0897344041d02f2de
Michael Nnaji <mnnaji3@uwo.ca>
2021-04-11 19:01:32 +0000

    Automatically backed up by Learn

--- db/migrate/20210411184737_create_landmarks.rb
+++ db/migrate/20210411184737_create_landmarks.rb
@@ -1,6 +1,6 @@
 class CreateLandmarks < ActiveRecord::Migration
   def change
-    create_tables :landmarks do |t|
+    create_table :landmarks do |t|
       t.string :name
       t.integer :figure_id
       t.integer :year_completed
